6fd4df8264dc89475782da7ddee677d1adc08f2b
achkatam <agmatevv@gmail.com>
2023-04-22 09:43:57 -0400

    images factored

---
+++ Data/Models/Car.cs
@@ -0,0 +1,13 @@
+﻿namespace CarServiceCRUD.Data.Models;
+
+public class Car
+{
+    public int Id { get; init; }
+    public string Make { get; init; } = null!;
+    public string Model { get; init; } = null!;
+    public int Year { get; init; }
+
+    // Keep the problem set, the other props init/ immutable. 
+    // You can always find more problems in a car, but cannot change its make and model
+    public string Problem { get; set; } = null!;
+}
